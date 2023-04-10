517a3eebca4941bc7cd3da29146340c646187d48
sagarxoxo <mss82605@gmail.com>
2023-04-10 16:34:09 +0530

    demo project search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [category, setCategory] = useState("none_selected");
+  const [allCategories, setAllCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  const [loading, setLoading] = useState(true);
+
+  const searchByCategory = () => {
+    category === "none_selected" ? getProducts() : getProductsByCategories();
+  };
+
+  console.log(category);
+
+  const resetProducts = () => {
+    getProducts();
+  };
+
+  const handleChange = (e) => {
+    setCategory(e.target.value);
+  };
+
+  async function getProducts() {
+    setLoading(true);
+    await axios("https://fakestoreapi.com/products")
+      .then((response) => setProducts(response.data))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }
+
+  async function getCategories() {
+    await axios("https://fakestoreapi.com/products/categories")
+      .then((response) => setAllCategories(response.data))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }
+
+  async function getProductsByCategories() {
+    setLoading(true);
+    await axios(`https://fakestoreapi.com/products/category/${category}`)
+      .then((response) => setProducts(response.data))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }
+
+  useEffect(() => {
+    getProducts();
+    getCategories();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Fake Store</h2>
+      <div className="btn-sec">
+        <select name="category" value={category} onChange={handleChange}>
+          <option value="none_selected">Select Category</option>
+          {allCategories.map((data, index) => (
+            <option key={index} value={data}>
+              {data}
+            </option>
+          ))}
+        </select>
+        <button onClick={searchByCategory}>Search</button>
+        <button onClick={resetProducts}>Reset</button>
+      </div>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <table className="table-main">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Title</th>
+              <th>Images</th>
+              <th>Price</th>
+              <th>Description</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product, index) => {
+              return (
+                <tr key={index}>
+                  <td>{product.id}</td>
+                  <td>{product.title}</td>
+                  <td>
+                    <img src={product.image} width="50px" />
+                  </td>
+                  <td>{product.price}</td>
+                  <td>{product.description}</td>
+                  <td>{product.category}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
